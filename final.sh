8b7bb4de22a6ca4c0716015713e4bad0a338fecb
magangwika <magangwika24@gmail.com>
2025-07-21 16:14:34 +0700

    Add files via upload

---
+++ final.sh
@@ -0,0 +1,1 @@
+python3 basic_pipelines/final_detection.py --hef-path resources/revisi1-small.hef --input url --labels-json resources/udin-labels.json 
---
+++ multiple.sh
@@ -0,0 +1,4 @@
+python3 basic_pipelines/coba-multiple.py --hef-path resources/revisi1-small.hef --input url --labels-json resources/udin-labels.json 
+
+
+
