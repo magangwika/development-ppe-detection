fe8751243b790deed33c4452fab4031e11835788
magangwika <magangwika24@gmail.com>
2025-07-21 16:23:15 +0700

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,5 @@
+# python3 basic_pipelines/app.py --hef-path qih-resources/qihsmall.hef --input qih-resources/last_reencoded.mp4 --labels-json  qih-resources/qih-labels.json 
+python3 basic_pipelines/app.py --hef-path qih-resources/apd8n.hef --input qih-resources/last_reencoded.mp4 --labels-json  qih-resources/apd8n.json 
+# python3 basic_pipelines/app.py --hef-path qih-resources/apd8n.hef --labels-json  qih-resources/apd8n.json  --input url
+# python3 basic_pipelines/app.py --hef-path qih-resources/qihsmall.hef --input url --labels-json  qih-resources/qih-labels.json 
+# python3 basic_pipelines/test.py --hef-path qih-resources/qihsmall.hef --input url --labels-json  qih-resources/qih-labels.json 
